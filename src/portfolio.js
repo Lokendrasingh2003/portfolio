b7e08d5cd2409ef1fd209c0309bc93d0fa04c3fe
Lokendrasingh2003 <rajawatlokendra2612@gmail.com>
2024-07-28 18:47:40 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,576 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello.",
+  title2: "Lokendra",
+  logo_name: "lokendra.s()",
+  nickname: "Ansh",
+  full_name: "Lokendra Singh",
+  subTitle:
+    "Full Stack Developer, Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1ImIFx4K3YNEhZJ8vpnq_MlbZiBv5ZoMf/view?usp=sharing",
+  mail: "mailto:rajawatlokendra2612@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Lokendrasingh2003",
+  linkedin: "https://www.linkedin.com/in/lokendra-singh-rajawat-071095280/",
+  gmail: "rajawatlokendra2612@gmail.com",
+  gitlab: " ",
+  facebook: " ",
+  twitter: "",
+  instagram: "https://www.instagram.com/ansh_singh.07/?next=%2F&hl=en",
+};
+
+const skills = {
+  data: [
+    {
+      title: "FrontEnd Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building visually stunning and user-friendly web experiences.",
+        "⚡ Building responsive website using HTML, CSS, JS, Tailwind And ReactJs",
+        
+        "⚡ Crafting responsive and interactive interfaces.",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+       
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NextJS",
+          fontAwesomeClassname: "simple-icons:nextdotjs",
+          style: {
+            color: "#FFFFFF",
+            backgroundColor: "#1D1D1D",
+            borderRadius: "50%",
+          },
+        },
+      
+      ],
+    },
+
+    {
+      title: "BackEnd Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Certified Django Developer. ",
+        "⚡ Experience working on multiple server-side applications.",
+        "⚡ Building robust and scalable server-side applications with Django.",
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+       
+     
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "ion-logo-python",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+        {
+          "skillName": "Django",
+          "fontAwesomeClassname": "simple-icons:django",
+          "style": {
+            "backgroundColor": "transparent",
+            "color": "#092E20"
+          }
+        }
+      ],
+    },
+
+
+    {
+      title: "Other Skills",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ C language: Where my coding journey began. ",
+        "⚡ Diving deep into object-oriented programming with C++.",
+        "⚡ Laying the groundwork in Java for future innovation.",
+        "⚡ From theory to practice: Implementing DSA in C++.",
+        "⚡ Git: Tracking changes and managing projects with precision",
+        "⚡ From data design to query execution: MySQL expertise.",
+        
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#A8B9CC",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#00599C",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "devicon:java",
+          style: {
+            color: "#007396",
+          },
+        },
+        
+        {
+          skillName: "DSA",
+          fontAwesomeClassname: "simple-icons:codeforces",
+          style: {
+            color: "#1F8ACB",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        
+      ],
+    },
+  
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "PS SOFTECH",
+      subtitle: "C PROGRAMMING CERTIFICATE",
+      logo_path: "",
+      alt_name: "",
+      duration: "July 2024",
+      descriptions: [
+        "⚡ Happy to share my latest achievement 🚀",
+        "⚡ Proud to share my certificate of completion in C PROGRAMMING at PS Softech! Excited for the new opportunities ahead.",
+        
+        "⚡ Excited to apply these skills in upcoming projects. 💡 ",
+      ],
+      website_link: "https://drive.google.com/file/d/1tS6MTv67q8WRRYDOFDKaAo_IF7Lz-5_F/view?usp=sharing",
+    },
+    {
+      title: "GURU NANAK INSTITUTIONS TECHNICAL CAMPUS",
+      subtitle: "HACKTOPIA",
+      logo_path: "dal.png",
+      alt_name: "DAL",
+      duration: "25 August - 26 August 2023",
+      descriptions: [
+        "⚡ Proud to have been part of Hackatopia! So many innovative ideas and great teamwork. 💡",
+        "⚡ Thrilled to have completed Hackatopia with such a fantastic team! 🌟",
+        "⚡ Wrapping up an intense and rewarding journey at Hackathopia! 🎉",
+      ],
+      website_link: "https://drive.google.com/file/d/1_UwDgF_g1zLZ07UrkK-NPVR8GAFjUfta/view?usp=sharing",
+    },
+    {
+      title: "I.T.M UNIVERSITY",
+      subtitle: "Smart India Hackathon",
+      logo_path: "",
+      alt_name: "",
+      duration: "September 2023",
+      descriptions: [
+        "⚡ Thrilled to have participated in the Smart India Hackathon!",
+        "⚡ An amazing experience at the Smart India Hackathon, where I honed my skills in managing work pressure and leading a team. 🚀",
+        "⚡Grateful for the experience! 🌟 ",
+      ],
+      website_link: "https://drive.google.com/file/d/1ivR-QwBlcPyeOy1KnW5J2ocjgUsRJTbq/view?usp=sharing",
+    },
+    {
+      title: "HackerRank",
+      subtitle: "SQL Certificate",
+      logo_path: "",
+      alt_name: "",
+      duration: "July 2024",
+      descriptions: [
+        "⚡ Excited to share that I’ve successfully cleared the SQL assessment on HackerRank! 💻",
+        "⚡ Happy to share my latest achievement: SQL certification on HackerRank! 🚀",
+        "⚡ Excited to apply these skills in upcoming projects. 💡 ",
+      ],
+      website_link: "https://drive.google.com/file/d/1ws2DgEgpuTmP6ZuPz8lsXgzSJ_KUUHdx/view?usp=sharing",
+    },
+
+    {
+      title: "PS SOFTECH",
+      subtitle: "PYTHON WITH DJANGO & REACT JS CERTIFIED",
+      logo_path: "",
+      alt_name: "",
+      duration: "July 2024",
+      descriptions: [
+        "⚡ Happy to share my latest achievement 🚀",
+        "⚡ Proud to share my certificate of completion in Python with Django and React JS from PS     Softech! Excited for the new opportunities ahead.",
+        
+        "⚡ Excited to apply these skills in upcoming projects. 💡 ",
+      ],
+      website_link: "https://drive.google.com/file/d/1UiBkEfwvb6BuW6xKjnj3B1z0fTEc3vbf/view?usp=sharing",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+   
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship ",
+  description:
+    "I've completed two internships. I've mostly done projects on my own and I am actively looking for internships. I love share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+   
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Full Stack Developer",
+          company: "Tech StudyCell",
+          company_url: "https://drive.google.com/file/d/1IeDJ48SNn5ucgL75zWnuaI0LjoOefY8M/view?usp=sharing",
+          logo_path: "mylog.png",
+          duration: "June 2023 - Aug 2023",
+          location: "Jhansi U.P",
+          description:
+            "I worked on the Dashboard project which helps users track their activities while using Automation System. Where anyone can track and control their electrical appliances",
+          color: "#ee3c26",
+        },
+        {
+          title: "Front-End Developer",
+          company: "The Website Makers",
+          company_url: "https://drive.google.com/file/d/1tgfA9Br2OglddRB5kubBBMTtw1G4-5yd/view?usp=sharing ",
+          logo_path: "buld.jpg",
+          duration: "November 2023 - December 2023",
+          location: "Work From Home",
+          description:
+            "Develop a website using ReactJS with great team. ",
+          color: "#0071C5",
+        },
+     
+        {
+          title: "Front-End Developer",
+          company: "VJ TechServe",
+          company_url: " ",
+          logo_path: "vjt.png",
+          duration: "September 2020",
+          location: "",
+          description: "Develop a portfolio website using ReactJS",
+          color: "#800000",
+        },
+      ],
+    },
+   
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Web Applications projects. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "myimage5.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "My Resume",
+    subtitle:
+      "Here's my journey in tech so far. Take a look at my resume to learn more about my work and skills.",
+    link: "https://drive.google.com/file/d/1ImIFx4K3YNEhZJ8vpnq_MlbZiBv5ZoMf/view?usp=sharing",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Seven_Shades",
+      url: "https://github.com/Lokendrasingh2003/Seven_Shades",
+      description:
+        "Fullstack Responsive e-commerce Website using reactjs , redux , Django , Restfull APIs and some other technologies",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Django",
+          iconifyClass: "vscode-icons:file-type-django",
+        },
+        {
+          name: "Material-UI",
+          iconifyClass: "logos-material-ui",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+
+    {
+      name: "Mail_Chimp",
+      url: "https://lokendrasingh2003.github.io/Mail_Chimp/",
+      description: "It is a clone website of MailChimp, a mail website. Using React.js, Full resposive client side website.",
+      languages: [
+       
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux"
+        },
+        {
+          name: "Chakra UI",
+          iconifyClass: "simple-icons:chakraui"
+        }
+        
+        
+
+       
+        
+        
+      ],
+    },
+    {
+      name: "Portfolio",
+      url: "",
+      description: "(This) A clean and full customizable portfolio template.",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      name: "Carters",
+      url: "https://github.com/Lokendrasingh2003/Carters",
+      description: "It is a e-commerce shopping website for kids. It is a full responsive and full functionality website using Reactjs,Redux,Next.js and some other technologies.",
+      languages: [
+       
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos-redux"
+        },
+        {
+          name: "Next.js",
+          iconifyClass: "simple-icons:nextdotjs"
+        },
+        
+        {
+          name: "Chakra UI",
+          iconifyClass: "simple-icons:chakraui"
+        }
+        
+        
+        
+        
+        
+        
+        
+      ],
+    },
+    {
+      name: "Cafaous",
+      url: "https://lokendrasingh2003.github.io/Cafaous/",
+      description: "Restaurants application.",
+      languages: [
+        {
+          "name": "HTML5",
+          "iconifyClass": "vscode-icons:file-type-html"
+        },
+        {
+          "name": "CSS3",
+          "iconifyClass": "vscode-icons:file-type-css"
+        },
+        {
+          "name": "JavaScript",
+          "iconifyClass": "logos-javascript"
+        },
+        {
+          "name": "Bootstrap",
+          "iconifyClass": "logos-bootstrap"
+        }
+        
+        
+      ],
+    },
+    {
+      name: "FUNEVT",
+      url: "https://lokendrasingh2003.github.io/FUNEVT/",
+      description:
+        "It is a client-side application where anyone can buy a ticket for singing conert.",
+      languages: [
+        {
+          "name": "HTML5",
+          "iconifyClass": "vscode-icons:file-type-html"
+        },
+        {
+          "name": "CSS3",
+          "iconifyClass": "vscode-icons:file-type-css"
+        },
+        {
+          "name": "JavaScript",
+          "iconifyClass": "logos-javascript"
+        },
+        {
+          "name": "Bootstrap",
+          "iconifyClass": "logos-bootstrap"
+        }
+      ],
+    },
+    {
+      name: "Wedding Planner",
+      url: "https://github.com/Lokendrasingh2003/Wedding_Planner.git",
+      description: "It is a web application where anyone can book their wedding destination and other facilities.",
+      languages: [
+        {
+          "name": "HTML5",
+          "iconifyClass": "vscode-icons:file-type-html"
+        },
+        {
+          "name": "CSS3",
+          "iconifyClass": "vscode-icons:file-type-css"
+        },
+        {
+          "name": "JavaScript",
+          "iconifyClass": "logos-javascript"
+        },
+      ],
+    },
+   
+   
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
